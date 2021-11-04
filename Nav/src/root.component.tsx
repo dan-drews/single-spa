947e3c4889b68a95f5016352d83777ae26092073
dan-drews <dan-drews@users.noreply.github.com>
2021-11-03 20:15:03 -0500

    Add Navigation and borders

--- Nav/src/root.component.tsx
+++ Nav/src/root.component.tsx
@@ -1,5 +1,6 @@
 export default function Root(props) {
-  return <section>
+  return <section style={{ border: "1px solid black" }}>
+    <a href="/" onClick={(e) => (window as any).singleSpaNavigate(e)}>Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     <a href="/POs" onClick={(e) => (window as any).singleSpaNavigate(e)}>POs</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     <a href="/COs" onClick={(e) => (window as any).singleSpaNavigate(e)}>COs</a>
   </section>;
